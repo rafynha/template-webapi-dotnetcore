19028c6cf014cc4e5a60034f64006c49d6ca51ff
Rafael Andrade <rafynha.and@gmail.com>
2020-03-02 09:49:28 -0300

    Inicio

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace t_dotnetcore_webapi.Controllers
+{
+    /// <summary>
+    /// Creates a TodoItem.
+    /// </summary>    
+    /// <response code="400">Requisição inválida</response>   
+    /// <response code="404">A página requerida é inexistente</response>   
+    /// <response code="500">Erro interno de servidor</response>            
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Listar itens.
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<WeatherForecast>))]
+        public IActionResult Get()
+        {
+            if (ModelState.IsValid)
+            {
+                var rng = new Random();
+                var retorno =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+                return Ok(retorno);  
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }                      
+        }
+    }
+}
